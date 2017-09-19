c5b670ae75962cebf30f116617e9d2fe0724a783
John <john@circlesabound.com>
2017-09-19 18:53:51 +1000

    Added a scrolling sidebar with clickable elements
    Added stub classes for front-end side logic components
    Added prefabs for front-end side logic components
    Added test sprites
    Modified directory structure a little

---
+++ Assets/Scripts/UI/UILogicComponentSelection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    /// <summary>
+    /// An element on the logic component selection panel
+    /// </summary>
+    public class UILogicComponentSelection : MonoBehaviour, IPointerClickHandler
+    {
+        private UILogicComponentSelectionPanel ComponentSelectionPanel;
+        public Sprite ActiveSprite
+        {
+            get;
+            private set;
+        }
+        public Sprite InactiveSprite
+        {
+            get;
+            private set;
+        }
+        public UILogicComponent LogicComponentPrefab;
+
+        // Use this for initialization
+        void Start()
+        {
+            this.ComponentSelectionPanel = this.GetComponentInParent<UILogicComponentSelectionPanel>();
+            Assert.IsNotNull(this.ComponentSelectionPanel);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            this.ComponentSelectionPanel.ToggleSelectedComponent(this);
+        }
+
+        public void SetSprites(Sprite activeSprite, Sprite inactiveSprite)
+        {
+            this.ActiveSprite = activeSprite;
+            this.InactiveSprite = inactiveSprite;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIOrGate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.UI
+{
+    public class UIOrGate : UILogicComponent
+    {
+        protected UIOrGate() : base()
+        {
+
+        }
+
+        // Use this for initialisation
+        void Start()
+        {
+            //TODO: attach a backend representation to this object
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
