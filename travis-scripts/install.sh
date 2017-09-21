1034a9dd9136b68ae05024d64e97b142accab1c9
John <john@circlesabound.com>
2017-09-22 01:57:22 +1000

    Attempt to make curl more robust

--- travis-scripts/install.sh
+++ travis-scripts/install.sh
@@ -3,7 +3,12 @@
 # Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 echo 'Downloading from http://netstorage.unity3d.com/unity/5d30cf096e79/MacEditorInstaller/Unity-2017.1.1f1.pkg: '
-curl --retry 2 -o Unity.pkg http://netstorage.unity3d.com/unity/5d30cf096e79/MacEditorInstaller/Unity-2017.1.1f1.pkg
+export ec=56 # connection reset by peer happens occasionally
+while [ $ec -eq 56 ]
+do
+    curl -O Unity.pkg -C - 'http://netstorage.unity3d.com/unity/5d30cf096e79/MacEditorInstaller/Unity-2017.1.1f1.pkg'
+    export ec=$?
+done
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
