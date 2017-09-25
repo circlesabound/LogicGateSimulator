e1466bdfa8460543b2ebea5ddaf5d50c3704dd4f
Ray Li <omnomnomtrees@gmail.com>
2017-09-25 10:27:50 +1000

    Added False and True Constant logic component.

---
+++ Assets/Scripts/Backend/LogicComponents/FalseConstant.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class FalseConstant : LogicComponent{
+    public FalseConstant() : base(0, 1) { }
+
+    public override List<bool> Simulate() => new List<bool> { false };
+}
---
+++ Assets/Scripts/Backend/LogicComponents/TrueConstant.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class TrueConstant : LogicComponent {
+    public TrueConstant() : base(0, 1) { }
+
+    public override List<bool> Simulate() => new List<bool> { true };
+}
