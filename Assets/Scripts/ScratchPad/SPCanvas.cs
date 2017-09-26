8823a4677aee596c298ab517ac9d5c3f4accbc0a
Nydrani <victorzhang6897@gmail.com>
2017-09-27 01:06:44 +1000

    Added element script

--- Assets/Scripts/ScratchPad/SPCanvas.cs
+++ Assets/Scripts/ScratchPad/SPCanvas.cs
@@ -10,25 +10,45 @@ namespace Assets.Scripts.ScratchPad
     {
         public GameObject elementPrefab;
         public GameObject foreground;
+        public Stack<GameObject> children;
 
         public void OnPointerClick(PointerEventData eventData)
         {
             if (eventData.button == PointerEventData.InputButton.Left)
             {
-                Debug.Log("Left click");
+                Debug.Log("Canvas| Left click");
+
                 GameObject newElem = GameObject.Instantiate(elementPrefab, eventData.pointerCurrentRaycast.worldPosition, Quaternion.identity);
+                newElem.tag = "SPElement";
+
+                // do this for now
                 newElem.transform.SetParent(foreground.transform);
                 newElem.transform.localScale = new Vector3(0.1f, 0.1f, 1);
+
+                // will this memory leak?
+                children.Push(newElem);
             }
             else if (eventData.button == PointerEventData.InputButton.Right)
             {
-                Debug.Log("Right click");
-                // open the right click menu here --> some youtube link
-                // this https://unity3d.com/learn/tutorials/modules/intermediate/live-training-archive/panels-panes-windows
+                Debug.Log("Canvas| Right click");
+
+                try
+                {
+                    GameObject oldElem = children.Pop();
+                    Destroy(oldElem);
+                } catch (InvalidOperationException e)
+                {
+                    Debug.Log("No children: " + e);
+                }
             }
         }
 
         // Use this for initialization
+        void Awake()
+        {
+            children = new Stack<GameObject>();
+        }
+
         void Start()
         {
             foreground = GameObject.Find("Foreground");
---
+++ Assets/Scripts/ScratchPad/SPElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.ScratchPad
+{
+    public class SPElement : MonoBehaviour, IPointerClickHandler
+    {
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left)
+            {
+                Debug.Log("Element| Left click");
+
+                // start drawing a line for connection?
+            }
+            else if (eventData.button == PointerEventData.InputButton.Right)
+            {
+                Debug.Log("Element| Right click");
+
+                // open the right click menu here --> some youtube link
+                // this https://unity3d.com/learn/tutorials/modules/intermediate/live-training-archive/panels-panes-windows
+            }
+        }
+
+        // Use this for initialization
+        void Awake()
+        {
+        }
+
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
