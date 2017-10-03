46493a851297bef1a2654718fe88356fa738af01
Austin Tankiang <austi.tanki@gmail.com>
2017-10-03 17:15:35 +1100

    Add backend output component object + tests

---
+++ Assets/Scripts/Backend/LogicComponents/Output.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class Output : LogicComponent {
+    public bool Value { get; private set; }
+
+    public Output() : base(1, 0) {
+    }
+
+    protected override List<bool> CoreSimulate(IList<bool> inputs)
+    {
+        this.Value = inputs[0];
+        return new List<bool> {};
+    }
+}
