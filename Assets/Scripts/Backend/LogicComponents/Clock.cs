40f790aa64d9e1d26334ed31737fd08e2a52dc2e
Austin Tankiang <austi.tanki@gmail.com>
2017-10-13 20:02:37 +1100

    Add clock backend component

---
+++ Assets/Scripts/Backend/LogicComponents/Clock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Clock : LogicComponent{
+    private uint tick;
+    private uint period;
+
+    public Clock(uint period) : base(0, 1) {
+        this.Outputs[0] = false;
+        this.tick = 0;
+        if (period == 0)
+        {
+            throw new ArgumentOutOfRangeException("Period cannot be zero");
+        }
+        this.period = period;
+    }
+
+    protected override List<bool> CoreSimulate(IList<bool> inputs)
+    {
+        this.tick++;
+        if (this.tick == period)
+        {
+            this.tick = 0;
+            return new List<bool> { !this.Outputs[0] };
+        }
+        return new List<bool> { this.Outputs[0] };
+    }
+}
