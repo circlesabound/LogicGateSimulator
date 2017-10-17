440ebaa4e174336fc6bc0e09e504316d2a4d51fb
Nydrani <victorzhang6897@gmail.com>
2017-10-17 15:44:31 +1100

    Added clock component

---
+++ Assets/Scripts/ScratchPad/SPClock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Assets.Scripts.ScratchPad
+{
+    public class SPClock : SPLogicComponent
+    {
+        protected SPConnector OutConnector
+        {
+            get
+            {
+                return OutConnectors[0];
+            }
+            set
+            {
+                OutConnectors[0] = value;
+            }
+        }
+
+        protected SPClock() : base()
+        {
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            OutConnectors.AddRange(Enumerable.Repeat<SPConnector>(null, 1));
+
+            // Set up connectors
+            OutConnector = Instantiate(SPOutConnectorPrefab, gameObject.transform, false);
+            Assert.IsNotNull(OutConnector);
+            OutConnector.gameObject.name = "OutConnector";
+            OutConnector.transform.localPosition = new Vector3(1, 0, -1);
+            OutConnector.Register(this, SPConnectorType.SPOutConnector, 0);
+        }
+
+        // Use this for initialisation
+        protected override void Start()
+        {
+            base.Start();
+            // @TODO hardcoded for now lmao
+            LogicComponent = new Clock(1);
+            Canvas.Circuit.AddComponent(LogicComponent);
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+        }
+    }
+}
