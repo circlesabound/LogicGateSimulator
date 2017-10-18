72d0d9284be7a9193b700f6028b5092eea39b143
John <john@circlesabound.com>
2017-10-18 13:29:13 +1100

    Implemented UI for setting clock component rate

---
+++ Assets/Scripts/UI/MessageBoxes/ClockMessageBox.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.Util;
+using System;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI.MessageBoxes
+{
+    public class ClockMessageBox : UIMessageBox
+    {
+        private float SliderValue
+        {
+            get
+            {
+                return gameObject.FindChildGameObject("UIMessageBox/UIMessageBoxNumberSliderContainer/UIMessageBoxNumberSlider").GetComponent<Slider>().value;
+            }
+        }
+
+        public void OnCancelButtonClick()
+        {
+            var triggerData = new MessageBoxTriggerData
+            {
+                ButtonPressed = MessageBoxButtonType.Negative,
+                Sender = this,
+            };
+            TriggerTarget.Trigger(triggerData);
+        }
+
+        public void OnConfirmButtonClick()
+        {
+            var triggerData = new MessageBoxTriggerData
+            {
+                ButtonPressed = MessageBoxButtonType.Positive,
+                Sender = this,
+                NumberInput = SliderValue
+            };
+            TriggerTarget.Trigger(triggerData);
+        }
+
+        public override void Trigger(MessageBoxTriggerData triggerData)
+        {
+            // shouldn't be accepting triggers
+            throw new NotImplementedException();
+        }
+    }
+}
